db924f12cbd8dcd781d881de341f8354211324e3
SoingJeang <soingjeang@gmail.com>
2022-07-21 16:36:04 +0800

    2_6 capture by cross 256 after last block

---
+++ contracts/CTFPredictTheBlockHashChallenge.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.21;
+
+contract CTFPredictTheBlockHashChallenge {
+    bytes32 public preGuess;
+    
+    function showMoreBlockHashAfter256() public view returns(bytes32) {
+        bytes32 answer = block.blockhash(block.number - 260);
+        return answer;
+    }
+
+    function showpreGuess() public view returns(bytes32){
+        return preGuess;
+    }
+
+    function setpreGuess(bytes32 _guess) public {
+        preGuess = _guess;
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 const ethers = require('ethers')
 
-const CTFPredictTheBlockHashChallenge = artifacts.require("CTFPredictTheBlockHashChallenge");
+const TokenSaleChallenge = artifacts.require("TokenSaleChallenge");
 
 module.exports = function(deployer) {
   //deployer.deploy(CTFPredictTheFuture, {value:ethers.utils.parseEther('1.0')});
-  deployer.deploy(CTFPredictTheBlockHashChallenge);
+  deployer.deploy(TokenSaleChallenge, "0x01d086b0448da1e41090a4839740c0eb7a7b861e", {value:ethers.utils.parseEther('1.0')});
 };
