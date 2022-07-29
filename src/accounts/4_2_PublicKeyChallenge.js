b1b24b2878b00a8f39c337d9b7cc8e0b9f2038e1
SoingJeang <soingjeang@gmail.com>
2022-07-29 16:16:02 +0800

    《深入理解以太坊EVM》 1、前言

--- src/accounts/4_2_PublicKeyChallenge.js
+++ src/accounts/4_2_PublicKeyChallenge.js
@@ -2,7 +2,7 @@
  * @Author: Soingjeang
  * @Date: 2022-07-21 14:49:07
  * @LastEditors: SoingJeang
- * @LastEditTime: 2022-07-28 19:04:23
+ * @LastEditTime: 2022-07-29 15:11:23
  * @FilePath: \CapTheEther\src\accounts\4_2_PublicKeyChallenge.js
  */
 const ethers = require('ethers');
@@ -13,12 +13,12 @@ const abiFile = './build/contracts/PublicKeyChallenge.json'; // fill
 const ctfAbi = './build/contracts/CTFPublicKeyChallenge.json'; // fill
 const secrteFile = "../.secret"
 const mnemonicFile = "../iBpnG3uuUwI.csv"
-const ctfaddress = "0x8467B2F569aA737a485471f74C8fBEa21Cb9eC35" // fill
+const ctfaddress = "0x7b7f01143514b21eeE4748DFe943d57196296069" // fill
 const contractChallangeAddress = "0x32b0c9020A63d17AEaE9d25d12bb6F41d24eeDd0" // fill
 const localChallange = "" //fill
-const localctf = "" //fill
+const localctf = "0x95d4881c150678Af330F395A5831B9387da74b94" //fill
 
-var localtest = 0  //fill
+var localtest = 1  //fill
 
 let provider = utils.getNetProvider("3", localtest)
 
@@ -37,7 +37,8 @@ async function guess(contractCtf, contractChallenge) {
     // 签名 [128:130]
     var v = ""
 
-    comp = await contractChallenge.isComplete()
+    // comp = await contractChallenge.isComplete()
+    comp= false
     console.log("complete: " + comp)
     if (!comp) {
         // for (var i=0; i < 1000000; i++) {
@@ -53,16 +54,46 @@ async function guess(contractCtf, contractChallenge) {
         //     }
         // }
         
-        let tranInfo = await provider.getTransaction(transactionHash)
+        // let tranInfo = await provider.getTransaction(transactionHash)
+        // console.log(tranInfo)
         
-        r = tranInfo.r
-        s = tranInfo.s
-        v = tranInfo.v
-        data = tranInfo.data
+        // r = tranInfo.r
+        // s = tranInfo.s
+        // v = tranInfo.v
+        // let data = tranInfo.data
 
-        console.log("r: " + r + "s: " + s + "v: " + v)
-        var pubAddr = contractCtf.doRecover(data, v + 27, r, s)
-        
+        // const expandedSig = {
+        //     r: r,
+        //     s: s,
+        //     v: v
+        //    }
+        // const signature = ethers.utils.joinSignature(expandedSig)
+           
+        // const txOriginData = {
+        //     gasPrice: tranInfo.gasPrice,
+        //     gasLimit: tranInfo.gasLimit,
+        //     value: tranInfo.value,
+        //     nonce: tranInfo.nonce,
+        //     data: data,
+        //     chainId: tranInfo.chainId,
+        //     to: tranInfo.to
+        // }
+        // const rsTx = await ethers.utils.resolveProperties(txOriginData)
+        // const rawTx = ethers.utils.serializeTransaction(rsTx)
+        // const msgHash = ethers.utils.keccak256(rawTx)
+        // const msgBytes = ethers.utils.arrayify(msgHash)
+        // const recoveredPubKey = ethers.utils.recoverPublicKey(msgBytes, signature)
+        // const recoveredAddress = ethers.utils.recoverAddress(msgBytes, signature)
+
+        // sendHash = ethers.utils.formatBytes32String(data)
+        // const newR = ethers.utils.arrayify(r)
+        // const newS = ethers.utils.arrayify(s)
+        r = '0xa5522718c0f95dde27f0827f55de836342ceda594d20458523dd71a539d52ad7'
+        s = '0x5710e64311d481764b5ae8ca691b05d14054782c7d489f3511a7abf2f5078962'
+        v = 41
+        msgHash = '0xd5059040fa47641a3388e7a6795eaaee42a96c6335189dd807075208adcb149a'
+        var pubAddr = await contractCtf.doRecoverByString(msgHash, r, s, v)
+        console.log(pubAddr)
     }
 
     console.log("uess me success")
